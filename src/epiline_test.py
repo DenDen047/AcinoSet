833e08f4ff408f0a845e3c72292efd65af70671c
DenDen047 <sh.mn.nat@gmail.com>
2021-07-12 11:38:53 +0900

    got the camera positions

--- src/epiline_test.py
+++ src/epiline_test.py
@@ -18,12 +18,26 @@ from pyomo.opt import SolverFactory
 
 from lib import misc, utils, app, vid
 from lib.calib import project_points_fisheye, triangulate_points_fisheye
-from lib.misc import get_markers
 
 import cv2 as cv
 
 
 plt.style.use(os.path.join('/configs', 'mplstyle.yaml'))
+
+
+def drawdots(img, pts, radius=3):
+    n_pts = len(pts)
+    colorclass = plt.cm.ScalarMappable(cmap='jet')
+    C = colorclass.to_rgba(np.linspace(0, 1, n_pts))
+    colors = (C[:, :3] * 255).astype(np.uint8).tolist()
+
+    for i, pt in enumerate(pts):
+        color = colors[i]
+        img = cv.circle(
+            img, tuple(pt.astype(np.uint16)),
+            radius=radius, color=color, thickness=-1
+        )
+    return img
 
 
 def epilines(DATA_DIR, points_2d_df, target_frame, target_bodypart, target_cam, epipolar_cam):
@@ -37,6 +51,7 @@ def epilines(DATA_DIR, points_2d_df, target_frame, target_bodypart, target_cam, 
     valid_frames = np.unique(points_2d_df['frame'])
     assert target_frame in valid_frames, print(f'choose from these frames: {valid_frames}')
     points_2d_df = points_2d_df[points_2d_df['frame'] == target_frame]
+
 
     # get video file paths
     candidate_cams = np.unique(points_2d_df['camera'])
@@ -52,29 +67,42 @@ def epilines(DATA_DIR, points_2d_df, target_frame, target_bodypart, target_cam, 
     image1 = clip1.load_frame()
     image2 = clip2.load_frame()
 
+    # draw the DLC points
+    pt = points_2d_df[points_2d_df['camera'] == cam_i][['x', 'y']].to_numpy()
+    image1 = drawdots(image1, pt, radius=5)
+    pt = points_2d_df[points_2d_df['camera'] == ecam_i][['x', 'y']].to_numpy()
+    image2 = drawdots(image2, pt, radius=5)
+
     # get 3d points
     epipoint_pos = utils.get_pairwise_3d_points_from_df(
         points_2d_df,
         k_arr, d_arr.reshape((-1,4)), r_arr, t_arr,
-        triangulate_points_fisheye
+        triangulate_points_fisheye,
+        verbose=False
     )[['x', 'y', 'z']].to_numpy().flatten()
-    epicam_pos = np.array(t_arr[ecam_i]).flatten()
-    print(epipoint_pos)
-    print(epicam_pos)
-    sys.exit(1)
-    points_3d = np.random.rand(1000, 3)
-    r = 3
-    points_3d[:, 0] = points_3d[:, 0] * r - r/2 + 8.0
-    points_3d[:, 1] = points_3d[:, 1] * r - r/2 + 5.5
-    points_3d[:, 2] = points_3d[:, 2] * r - r/2 + 0.0
+    camera_positions = misc.global_positions(r_arr, t_arr)
+    epicam_pos = camera_positions[ecam_i].flatten()
+
+    n = 1000
+    points = []
+    for i in range(n+1):
+        r = i / n
+        p = r*epicam_pos + (1-r)*epipoint_pos
+        points.append(p)
+
+    points_3d = np.vstack(points)
 
     # project 3d points to 2d ones
     pts1 = project_points_fisheye(points_3d, k_arr[cam_i], d_arr[cam_i], r_arr[cam_i], t_arr[cam_i])
     pts2 = project_points_fisheye(points_3d, k_arr[ecam_i], d_arr[ecam_i], r_arr[ecam_i], t_arr[ecam_i])
 
+    # draw points
+    result1 = drawdots(image1, pts1, radius=1)
+    result2 = drawdots(image2, pts2, radius=1)
+
     # save
-    cv.imwrite(os.path.join(OUT_DIR, f'frame_{target_frame}_1.jpg'), result1)
-    cv.imwrite(os.path.join(OUT_DIR, f'frame_{target_frame}_2.jpg'), result2)
+    cv.imwrite(os.path.join(OUT_DIR, f'frame_{target_frame}_{target_cam}.jpg'), result1)
+    cv.imwrite(os.path.join(OUT_DIR, f'frame_{target_frame}_{epipolar_cam}.jpg'), result2)
 
 
 def dlc(DATA_DIR, OUT_DIR, dlc_thresh, params: Dict = {}) -> Dict:
@@ -94,10 +122,7 @@ def dlc(DATA_DIR, OUT_DIR, dlc_thresh, params: Dict = {}) -> Dict:
 if __name__ == '__main__':
     parser = ArgumentParser(description='All Optimizations')
     parser.add_argument('--data_dir', type=str, help='The file path to the flick/run to be optimized.')
-    parser.add_argument('--start_frame', type=int, default=1, help='The frame at which the optimized reconstruction will start.')
-    parser.add_argument('--end_frame', type=int, default=-1, help='The frame at which the optimized reconstruction will end. If it is -1, start_frame and end_frame are automatically set.')
     parser.add_argument('--dlc_thresh', type=float, default=0.8, help='The likelihood of the dlc points below which will be excluded from the optimization.')
-    parser.add_argument('--plot', action='store_true', help='Show the plots.')
     args = parser.parse_args()
 
     mode = 'head'
@@ -112,9 +137,6 @@ if __name__ == '__main__':
         'vid_fps': fps,
         'total_frames': num_frames,
     }
-    assert 0 < args.start_frame < num_frames, f'start_frame must be strictly between 0 and {num_frames}'
-    assert 0 != args.end_frame <= num_frames, f'end_frame must be less than or equal to {num_frames}'
-    assert 0 <= args.dlc_thresh <= 1, 'dlc_thresh must be from 0 to 1'
 
     # generate labelled videos with DLC measurement data
     DLC_DIR = os.path.join(DATA_DIR, 'dlc_head')
@@ -127,6 +149,7 @@ if __name__ == '__main__':
     # D ... lens distortion
     # R, T ... the extrinsic parameters which denote the coordinate system transformations from 3D world coordinates to 3D camera coordinates
     k_arr, d_arr, r_arr, t_arr, cam_res, n_cams, scene_fpath = utils.find_scene_file(DATA_DIR, verbose=False)
+    print('scene file:', scene_fpath)
     assert res == cam_res
     camera_params = (k_arr, d_arr, r_arr, t_arr, cam_res, n_cams)
     # load DLC data
@@ -140,21 +163,11 @@ if __name__ == '__main__':
     assert len(k_arr) == points_2d_df['camera'].nunique()
 
     # Triangulation
-    _ = epilines(
+    epilines(
         DATA_DIR, filtered_points_2d_df,
         target_frame=137,
         target_bodypart='r_eye',
         target_cam=2,
         epipolar_cam=3
     )
-    plt.close('all')
 
-    if args.plot:
-        print('Plotting results...')
-        data_fpaths = [
-            os.path.join(DATA_DIR, 'tri', 'tri.pickle'),    # plot is too busy when tri is included
-            os.path.join(DATA_DIR, 'sba', 'sba.pickle'),
-            os.path.join(DATA_DIR, 'ekf', 'ekf.pickle'),
-            os.path.join(DATA_DIR, 'fte', 'fte.pickle')
-        ]
-        app.plot_multiple_cheetah_reconstructions(data_fpaths, reprojections=False, dark_mode=True)
