55cfeebb580c518c89c51e5e462c80820bbf6a42
DenDen047 <sh.mn.nat@gmail.com>
2021-10-11 14:26:48 +0900

    output DLC likelihood scores

--- src/core/dlc.py
+++ src/core/dlc.py
@@ -1,13 +1,16 @@
-import os
+import os, sys
 import json
+import pickle
 import pandas as pd
+from pprint import pprint
 from typing import Dict, List
 from glob import glob
+from scipy.io import savemat
 
-from lib import app
+from lib import app, misc
 
 
-def dlc(DATA_DIR, OUT_DIR, dlc_thresh, params: Dict = {}) -> Dict:
+def dlc(DATA_DIR, OUT_DIR, mode, dlc_thresh, params: Dict = {}, video: bool = False) -> Dict:
     df_fpaths = sorted(glob(os.path.join(OUT_DIR, 'cam[1-9]*.h5'))) # original vids should be in the parent dir
     video_fpaths = sorted(glob(os.path.join(DATA_DIR, 'cam[1-9].mp4'))) # original vids should be in the parent dir
 
@@ -22,6 +25,26 @@ def dlc(DATA_DIR, OUT_DIR, dlc_thresh, params: Dict = {}) -> Dict:
         df = pd.read_hdf(df_fpath)
         point2d_dfs.append(df)
 
-    app.create_labeled_videos(point2d_dfs, video_fpaths, out_dir=OUT_DIR, draw_skeleton=True, pcutoff=dlc_thresh, lure=False)
+    # extract likelihoods
+    markers = misc.get_markers(mode)
+    likelihood_data = {}
+    for i, df in enumerate(point2d_dfs):
+        df = df.loc[:,('DLC_resnet152_CheetahOct14shuffle1_500000')]
+        likelihoods = df.loc[:, (markers, 'likelihood')]
+        likelihoods.columns = likelihoods.columns.droplevel('coords')
+        r = {name: col.values for name, col in likelihoods.items()}
+        likelihood_data[f'cam{i+1}'] = r
+
+    # save
+    data_fpath = os.path.join(OUT_DIR, 'dlc')
+    pkl_fpath = data_fpath + '.pickle'
+    with open(pkl_fpath, 'wb') as f:
+        pickle.dump(likelihood_data, f)
+    mat_fpath = data_fpath + '.mat'
+    savemat(mat_fpath, likelihood_data)
+
+    # video
+    if video:
+        app.create_labeled_videos(point2d_dfs, video_fpaths, out_dir=OUT_DIR, draw_skeleton=True, pcutoff=dlc_thresh, lure=False)
 
     return params
